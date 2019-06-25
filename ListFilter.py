9cb5a1ed739e41a14cd91cd36714c374e76330c7
Sridaran <sri7thon@gmail.com>
2019-06-24 22:22:05 -0400

    Filtered WordListMedium

--- ListFilter.py
+++ ListFilter.py
@@ -6,8 +6,8 @@ def check_word(word):
     return length > MIN_WORD_LENGTH and length < MAX_WORD_LENGTH
 
 def main():
-    with open('WordList20k', 'r') as word_list:
-        with open('FilteredWordList20k', 'w+') as filtered_word_list:
+    with open('WordListMedium', 'r') as word_list:
+        with open('FilteredWordListMedium', 'w+') as filtered_word_list:
             for word in word_list:
                 if check_word(word):
                     filtered_word_list.write(word)
